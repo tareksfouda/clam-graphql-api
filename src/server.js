173fb446c3e78d86b8396264c8c5cdb1b4eee436
Alex Banks <alex@moonhighway.com>
2018-10-26 11:38:16 -0700

    Temporarly fixes issue (#39)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
 import { ApolloServer } from 'apollo-server'
 import resolvers from './resolvers'
-import typeDefs from './typeDefs.graphql'
+
+/* Importing TypeDefs is a little brittle
+
+   They get cached and slow down our workflow. More on the issue here:
+   https://github.com/detrohutt/babel-plugin-import-graphql/issues/47
+
+   We would like to simply import the typeDefs directly and have any changes
+   instantly appear. At present, for that we must read the file directly
+   until there is a better solution.
+
+*/
+
+// Temporary solution
+import { readFileSync } from 'fs'
+const typeDefs = readFileSync('./src/typeDefs.graphql', 'UTF-8')
+
+// import typeDefs from './typeDefs.graphql'
+
+console.log(typeDefs)
 
 export const start = async () => {
   const engine = process.env.ENGINE_API_KEY
