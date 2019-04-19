de10c019a9b31cb09292f83deca6fb1a44514676
MoonTahoe <alex@moonhighway.com>
2019-04-19 15:26:50 -0700

    Attempting to fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 var webpack = require('webpack')
 var nodeExternals = require('webpack-node-externals')
 var path = require('path')
-require('@babel/polyfill')
 
 module.exports = {
   target: 'node',
   externals: [nodeExternals()],
-  entry: ['@babel/polyfill', './src/index.js'],
+  entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'index.js',
